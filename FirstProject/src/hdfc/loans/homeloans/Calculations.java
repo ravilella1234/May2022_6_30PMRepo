a8e605d901ca548c2f086b9183c0457ace38ed0f
ravilella1234 <masters.selenium@gmail.com>
2022-05-13 19:38:31 +0530

    erewrewr

--- FirstProject/src/hdfc/loans/homeloans/Calculations.java
+++ FirstProject/src/hdfc/loans/homeloans/Calculations.java
@@ -1,34 +1,61 @@
 package hdfc.loans.homeloans;
 
+import java.util.Scanner;
+
+/* Java Program Example - Make Calculator */
+
 public class Calculations 
 {
-	int a,b,c;
 
-	public Calculations(int a, int b) 
-	{
-		super();
-		this.a = a;
-		this.b = b;
-	}
-	
-	public void add()
-	{
-		c = a+b;
-		System.out.println("Add of A & B is : " +  c);
-	}
-	
-	public void sub()
-	{
-		c = a-b;
-		System.out.println("Sub of A & B is : " +  c);
-	}
-	
-	
 	public static void main(String[] args) 
 	{
-		Calculations obj = new Calculations(10, 20);
-		obj.add();
-		obj.sub();
+		float a, b, res;
+        char choice;
+        Scanner scan = new Scanner(System.in);
+		
+        do
+        {
+            System.out.print("1. Addition\n");
+            System.out.print("2. Subtraction\n");
+            System.out.print("3. Multiplication\n");
+            System.out.print("4. Division\n");
+            System.out.print("5. Exit\n\n");
+            System.out.print("Enter Your Choice : ");
+            choice = scan.next().charAt(0);
+            switch(choice)
+            {
+                case '1' : System.out.print("Enter Two Number : ");
+                    a = scan.nextFloat();
+                    b = scan.nextFloat();
+                    res = a + b;
+                    System.out.print("Result = " + res);
+                    break;
+                case '2' : System.out.print("Enter Two Number : ");
+                    a = scan.nextFloat();
+                    b = scan.nextFloat();
+                    res = a - b;
+                    System.out.print("Result = " + res);
+                    break;
+                case '3' : System.out.print("Enter Two Number : ");
+                    a = scan.nextFloat();
+                    b = scan.nextFloat();
+                    res = a * b;
+                    System.out.print("Result = " + res);
+                    break;
+                case '4' : System.out.print("Enter Two Number : ");
+                    a = scan.nextFloat();
+                    b = scan.nextFloat();
+                    res = a / b;
+                    System.out.print("Result = " + res);
+                    break;
+                case '5' : System.exit(0);
+                    break;
+                default : System.out.print("Wrong Choice!!!");
+                    break;
+            }
+            System.out.print("\n---------------------------------------\n");
+        }while(choice != 5);       
+
 	}
 
 }
